772ef69ee878d307a3b938f48cf835a2472e95f2
michele <michele@5cdbaba4-1eeb-449d-b79e-0e51767f5157>
2015-04-30 21:11:05 +0000

    renamed weatherfetch to openweathemap.js. Added swissmetnet.js
    git-svn-id: svn://192.168.1.4/cellarduino@35 5cdbaba4-1eeb-449d-b79e-0e51767f5157

---
+++ weatherfetch/app/swissmetnet.js
@@ -0,0 +1,46 @@
+var http = require('http');
+
+var options = {
+  host: '127.0.0.1',
+  port: 3001,
+  path: '/sensors/swissmetnet/data',
+  method: 'PUT',
+  headers: {'Content-Type': 'application/json'}
+};
+
+
+http.get("http://data.netcetera.com/smn/smn/SMA", function(res) {
+  
+    var body = '';
+
+    res.on('data', function(chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function() {
+      var data = JSON.parse(body);
+      sendWeatherDataToServer(data.temperature, data.humidity);
+    });
+
+
+  }).on('error', function(e) {
+    console.log("Got error: " + e.message);
+});
+
+function sendWeatherDataToServer(temperature, humidity) {
+  var currentConditions = {};
+  currentConditions.temperature = temperature;
+  currentConditions.humidity = humidity;
+
+  var request = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+
+  request.write(JSON.stringify(currentConditions));
+  request.end();
+}
